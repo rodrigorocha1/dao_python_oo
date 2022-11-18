6973975c4d55d75821b3f8f2536ae9d02e89eac8
rodrigorocha1 <silva.rodrigo31@outlook.com>
2022-11-17 21:32:24 -0300

    chamada da função de fabrica

--- dao/msql_dao.py
+++ dao/msql_dao.py
@@ -1,24 +1,28 @@
-from idao import IDao
+from dao.idao import IDao
 from entidades.vendedor_ouro import VendedorOuro
 from dao.conexao_fabrica import ConexaoFabrica
 import mysql.connector.errors as error
 
-
 class MysqlDAO(IDao):
     def inserir(self, vendedor_ouro: VendedorOuro):
-        try:
-            conexao = ConexaoFabrica()
-            cursor = conexao.conection_factory()
-            sql_insert = f'INSERT INTO vendedor (id_vendedor, nome, segmento)' \
-                         f'VALUES ({vendedor_ouro.id_vendedor}, "{vendedor_ouro.nome}", "{vendedor_ouro.segmento}")'
-            cursor.execute(sql_insert)
-            conexao.commit(cursor)
-        except error.DatabaseError as database_erro:
-            print(
-                f'Erro ao inserir o registro {vendedor_ouro.id_vendedor}, {vendedor_ouro.nome}, {vendedor_ouro.segmento}')
-            print(database_erro)
-            pass
 
+        try:
+            c = ConexaoFabrica()
+            conexao, cursor = c.conection_factory()
+
+            sql_insert = f'INSERT INTO vendedor (id_vendedor, nome, segmento) ' \
+                         f'VALUES ({vendedor_ouro.id_vendedor}, "{vendedor_ouro.nome}", "{vendedor_ouro.segmento}")'
+            print(sql_insert)
+            cursor.execute(sql_insert)
+            conexao.commit()
+            print('Registro Inserido')
+
+        except error.DatabaseError as database_erro:
+            print(f'Erro ao inserir o registro {vendedor_ouro.id_vendedor}, {vendedor_ouro.nome}, {vendedor_ouro.segmento}')
+            print(database_erro)
+        except error.DataError as desc_erro:
+            print(f'Erro ao inserir o registro {vendedor_ouro.id_vendedor}, {vendedor_ouro.nome}, {vendedor_ouro.segmento}')
+            print(desc_erro)
         finally:
             cursor.close()
 
