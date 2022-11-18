f0abeba604e947fa3c40f7e40dd710d3050c442b
rodrigorocha1 <silva.rodrigo31@outlook.com>
2022-11-17 21:31:47 -0300

    menu com dados de parametros

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from entidades.vendedor_ouro import VendedorOuro
+from dao.vendedordao import VendedorOuroDAO
+from dao.msql_dao import MysqlDAO
 
 while True:
+
     op = int(input('1 - Inserir \n'
                    ''))
     if op == 1:
-        inp_id = int(input('Digite o código do vendedor'))
-        inp_nm = int(input('Digite o nome do vendedor'))
-        inp_seg = int(input('Digite o segmento do vendedor'))
-
+        # inp_id = int(input('Digite o código do vendedor: '))
+        # inp_nm = (input('Digite o nome do vendedor: '))
+        # inp_seg = (input('Digite o segmento do vendedor: '))
+        inp_id = 3
+        inp_nm = 'vend3'
+        inp_seg = 'ouro'
+        vendedordao = VendedorOuroDAO(MysqlDAO())
         vo = VendedorOuro(inp_id, inp_nm, inp_seg)
-
-
-
+        vendedordao.inserir(vo)
     else:
         break
